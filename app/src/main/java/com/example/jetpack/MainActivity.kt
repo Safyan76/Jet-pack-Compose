97268d10905bbecd4f7ad08999516bd4425c3d97
safyan <safyananwar76@gmail.com>
2026-02-28 12:24:05 +0500

    Button added through JPC Method

--- app/src/main/java/com/example/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.example.jetpack
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.jetpack.ui.theme.JetpackTheme
 
@@ -23,39 +34,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackTheme {
-                Scaffold (modifier = Modifier.fillMaxSize()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = MaterialTheme.colorScheme.background,
                 ) { innerPadding ->
-                    Box(
-                        modifier = Modifier.fillMaxSize()
-                            .padding(
-                                innerPadding
-                            ),
-                    contentAlignment = Alignment.Center){
-                        Greeting(
-                            name = "Android"
-                        )
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        RoundedButton()
+                        composeimage()
                     }
-
-                    Text(text = "Hello World!")
-
                 }
             }
         }
     }
+
 }
+@Composable
+/*fun composeimage(){
+    Image(painter = painterResource(id = R.drawable.logo), contentDescription = "Borcelle Logo")
+}*/
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackTheme {
-        Greeting("Android")
+private fun RoundedButton() {
+    val context = LocalContext.current
+    Button(
+        onClick = { Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show() },
+        shape = RoundedCornerShape(size = 15.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+    ) {
+        Text(text = "Click Me")
     }
 }
